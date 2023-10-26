a77d18763fca4ba66ddb6a4f9022ccab34fedd5a
Std137 <61679468+Std137@users.noreply.github.com>
2023-10-26 22:57:01 +0700

    refactoring
    Full refactoring source code

--- Source/Full.py
+++ Source/Full.py
@@ -1,12 +1,15 @@
-from tkinter import *           #подключаем библиотеку управления окнами и виджетами.
-from tkinter import messagebox  #подключаем библиотеку управления диологовых окон.
+import tkinter as tk        #подключаем библиотеку управления окнами и виджетами.
+from tkinter import ttk as ttk  #подключаем библиотеку управления диологовых окон.
 import random                   #подключаем библиотеку random.
 
 col = ['red','green','blue','grey']
 
 '''
 --------------------------------------------------------------------------------------------------
-class Briks - предназначен для хранения параметров отдельных кирпичей. Используется в классе Wall
+class Briks - предназначен для хранения параметров отдельных кирпичей. Используется в классе Wall.
+Конструктор Briks() - вызов без аргументов
+Функция GetColor() - аргументов нет. Возвращает цвет текущего кирпича.
+Функция BriksStatus() - аргументов нет. Возвращает статус текущего кирпича, 0 - нет, 1 - есть.
 --------------------------------------------------------------------------------------------------
 '''
 
@@ -24,73 +27,122 @@ class Briks:
 	def BriksOff(self):
 		self.status = 0
 
+'''
+--------------------------------------------------------------------------------------------------
+class Briks - предназначен для хранения параметров отдельных кирпичей. Используется в классе Wall
+--------------------------------------------------------------------------------------------------
+'''
+class Wall:
+	bricks_wall = {}
+	def __init__(self):
+		for strok in range(7):
+			brick_stroka = {}
+			for stolb in range(9):
+				brick_stroka[stolb] = Briks()
+			bricks_wall[strok] = brick_stroka
 
-class Field:
-	holst = 0
-	def __init__(self,canvas):
-		self.holst = canvas
-		self.GameField = []
-		for i in range(13):
-			for	we in range(7):
-				self.GameField.append(Briks((49*i),20*we))			
+	def wall_touch(self, stolbec, stroka):
+		pass
 
-	def ShowField(self):
-		for j in self.GameField:
-			if j.BriksStatus() == 1:
-				self.holst.create_rectangle(j.BriksPositionX(), j.BriksPositionY(), j.BriksPositionX()+49, j.BriksPositionY()+20, fill=j.RetColor(), outline=out_color)
+	def refresh(self):
+		pass
+
+class Ball:
+	def __init__(self, canvas):
+		self.canvas = canvas
+		self.x = 320
+		self.y = 200
+		self.dx = -1
+		self.dy = -1
+		self.color = 'green'
+		self.radius = 10
+		self.canvas.create_oval(310, 460, 330, 480 , fill= self.color, outline="white", tag = 'ball')
 
 
+	def get_move(self):
+		return [self.x,self.y,self.dx,self.dy]
 
-class Board:
-	boardposx = 0
-	boardposy = 0
-	brush = 0
-	board = 0
-	def __init__(self,canvas):
-		self.boardposx = 320
-		self.boardposy = 440
-		self.brush = canvas
-		self.board = self.brush.create_rectangle(self.boardposx-50, self.boardposy, self.boardposx+50, self.boardposy+20, fill='green')
+	def get_param(self):
+		return [self.color, self.radius]
 
-	def MoveBoard(self):
-		self.brush.coords(self.board,self.boardposx-50, self.boardposy, self.boardposx+50, self.boardposy+20)
+class Move_Ball:
+	fps = 10
+	def __init__(self, window, canvas, ball):
+		self.canvas = canvas
+		self.window = window
+		#self.wall = wall
+		#self.rect = rect
+		#self.board = board
+		#self.bottom_level = bottom_level
+		self.ball = ball 
+
+	def render(self):
+		self.canvas.move('ball', -1, -1)
 		
-	def SetPosition(self,xpos,ypos):
-		self.boardposx = xpos
-		self.boardposy = ypos
-	def GetPosition(self):
-		return [self.boardposx, self.boardposy]
 
-class GameInit:
-	windows = 0
-	holsts = 0
-	def __init__(self):
-		self.windows = Tk()
-		self.windows.title("Сбежавший мячик!!!")                        #Выводим название программы в окне
-		self.windows.geometry('640x480+{}+{}'.format(((self.windows.winfo_screenwidth()//2)-320), ((self.windows.winfo_screenheight()//2)-240)))
-		self.windows.resizable(0,0)                                     #Запрет изменния рзмера окна
-		self.holsts = Canvas(self.windows, width=640, height=480, bg='black')
-	def getholst(self):
-		return self.holsts
-	def getwindow(self):
-		return self.windows
+	def test(self):
+		self.x, self.y, self.dx, self.dy = self.ball.get_move()
+		self.new_x = self.x + self.dx
+		self.new_y = self.y + self.dy
+		#if (self.wall.wall_touch()) or (self.rect.rect_touch()) or (self.board.board_touch()):
+		#	self.new_path()
+		#if (self.bottom_level.bottom_touch()):
+		#	self.game_over()
+		self.render()
+		self.canvas.after(self.fps, self.test)
 
-class game GameMode:
+class Start_Game(tk.Toplevel):
+	def __init__(self, parent):
+		super().__init__(parent)
+		self.title("Мочи кирпчи")
+		self.geometry('640x480+{}+{}'.format(((self.winfo_screenwidth()//2)-320), ((self.winfo_screenheight()//2)-240)))
+		self.resizable(0,0)
+		self.holsts = tk.Canvas(self, width=640, height=480, bg='black')
+		self.holsts.pack()
+		self.ball = Ball(self.holsts)
+		self.movie_ball = Move_Ball(self, self.holsts, self.ball)
+		self.movie_ball.test()
+
+
+class Score_win(tk.Toplevel):
+	def __init__(self, parent):
+		super().__init__(parent)
+		self.style = ttk.Style()
+		self.style.configure('my.TButton', font=('Bauhaus 93', 24, 'bold'), borderwidth='10', borderradius='5')
+		self.style.map('my.TButton', foreground=[('pressed', 'blue'), ('active', 'green')], background=[('pressed', '!disabled', 'black'), ('active', 'red')])
+		self.style.configure('my.TLabel', font=('Bauhaus 93', 24, 'bold'), borderwidth='10', borderradius='5')
+		self.style.map('my.TLabel')
+		self.head_name = ttk.Label(self, style= 'my.TLabel', text='Arkanoid @2023').pack(ipady = 20)
+		self.btn_quit = ttk.Button(self, style= 'my.TButton', text='Quit', command=lambda: self.destroy()).pack()
+
+
+class Start_win(tk.Tk):
 	def __init__(self):
-		gm = GameModel()
-		gm.getwindow().mainloop()
-	def Start():
-	def Game():
-	def GameOver():
-x_start=320         #создаём переменную и задаём текущее положение мяча по координате X.
-y_start=240         #создаём переменную и задаём текущее положение мяча по координате Y.
-stepX=0             #создаём переменную и задаём шаг движения вправо.
-stepY=0             #создаём переменную и задаём шаг движения вверх.
-fill_color = 'green'  # устанавливаем заливку круга красным.
-out_color = 'white' # устанавливаем заливку круга красным.
-gm = GameModel()
-Game = Field(gm.getholst())
-Game.ShowField()
-boards= Board(gm.getholst())
-gm.getholst().pack()
-                   
+		super().__init__()
+		self.title("Арканоид")
+		self.geometry('320x240+{}+{}'.format(((self.winfo_screenwidth()//2)-160), ((self.winfo_screenheight()//2)-120)))
+		self.resizable(0,0)
+		self.style = ttk.Style()
+		self.style.configure('my.TButton', font=('Bauhaus 93', 24, 'bold'), borderwidth='10', borderradius='5')
+		self.style.map('my.TButton', foreground=[('pressed', 'blue'), ('active', 'green')], background=[('pressed', '!disabled', 'black'), ('active', 'red')])
+		self.style.configure('my.TLabel', font=('Bauhaus 93', 24, 'bold'), borderwidth='10', borderradius='5')
+		self.style.map('my.TLabel')
+		self.head_name = ttk.Label(self, style= 'my.TLabel', text='Arkanoid @2023').pack(ipady = 20)
+		self.btn_start = ttk.Button(self, style= 'my.TButton', text='StarT', command=self.clck_btn_start).pack()
+		self.btn_score = ttk.Button(self, style= 'my.TButton', text='Score', command=self.clck_btn_score).pack()
+		self.btn_quit = ttk.Button(self, style= 'my.TButton', text='Quit', command=lambda: self.destroy()).pack()
+	
+	def clck_btn_start(self):
+		game_clk = Start_Game(self)
+		game_clk.grab_set()
+		
+	def clck_btn_score(self):
+		score_clk = Score_win(self)
+		score_clk.grab_set()
+
+class Game_win:
+	def __init__(self):
+		pass
+
+game = Start_win()
+game.mainloop()
